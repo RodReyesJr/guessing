d0c4424fa5fa857669f2cc686a0bb4c8ebfda1cf
RodReyesJr <reyesrodjr@gmail.com>
2017-11-20 19:27:00 -0800

    Added try..except

--- guess.py
+++ guess.py
@@ -8,23 +8,28 @@ def main():
     maxguess = 5 # max guess count allowed
     numguess = 0 # guess counter
     while True:
-        guess = input("Guess a number between one and ten. Type 'q' to quit: ")
-        if guess == 'q':
-            break
+        guesses = input("Guess a number between one and ten. Type 'q' to quit: ")
+        
+        
+        try: 
+            if guesses == 'q':
+                break         
+            guess = int(guesses)
+            if int(guess) == number:
+                print("That's right!")
+                time.sleep(3) # delay for awhile
+                break
+            elif int(guess) > number:
+                print("That's not right. Too high! Sorry! Guess again.")
+                numguess = numguess + 1
+            elif int(guess) < number:
+                print("That's not right. Too low! Sorry! Guess again.")
+                numguess = numguess + 1
 
-        if int(guess) == number:
-            print("That's right!")
-            time.sleep(3) # delay for awhile
-            break
-        elif int(guess) > number:
-            print("That's not right. Too high! Sorry! Guess again.")
-            numguess = numguess + 1
-        elif int(guess) < number:
-            print("That's not right. Too low! Sorry! Guess again.")
-            numguess = numguess + 1
-
-        if numguess == maxguess:
-            break # quit now if maximum guess is reached
+            if numguess == maxguess:
+                break # quit now if maximum guess is reached
+        except ValueError:
+            print("\nPlease enter a number between 1 to 10. Press 'q' to quit.")
 
 if __name__ == '__main__':
     main()
